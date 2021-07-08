29b641f6a7e5e86572d83394a2acce961b785c57
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-07-08 15:46:00 -0300

    ajustando deletar pedido

--- dynamodb_pedido/src/controller/deleta.js
+++ dynamodb_pedido/src/controller/deleta.js
@@ -1,14 +1,31 @@
 const dao = require('../dao/pedido_dao');
 
 module.exports.deletar = async (event) => {
+   const { numero_pedido } = JSON.parse(event.body);
+
    const params = {
       TableName: 'Pedido',
       Key: {
-         "numero_pedido": "00"
+         'numero_pedido': numero_pedido
       }
    };
 
-   const r = await dao.deletar(params)
-   console.log(r)
+   let response = {
+      statusCode: 0,
+      body: ''
+   }
+
+   try {
+      const items = await dao.deletar(params)
+
+      response.statusCode = 204
+   } catch (error) {
+      console.log(error)
+
+      response.statusCode = 500
+      response.body = JSON.stringify({ msg: 'Falha em algo.' })
+   }
+   
+   console.log(response)
    return
 }
