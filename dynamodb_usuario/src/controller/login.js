c3739898ea8ab0e0e801545024bf60f51cc2fbe7
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-07-27 09:23:04 -0300

    usuario: refatorando login

--- dynamodb_usuario/src/controller/login.js
+++ dynamodb_usuario/src/controller/login.js
@@ -18,8 +18,8 @@ module.exports.login = async event => {
    } catch (error) {
       console.error(error)
 
-      response.statusCode = 500
-      response.body = JSON.stringify({ msg: 'Falha em algo.' })
+      response.statusCode = error.statusCode || 500
+      response.body = JSON.stringify({ error: error.msg || 'Algo deu errado' })
    }
 
    return response
--- dynamodb_usuario/src/service/login.js
+++ dynamodb_usuario/src/service/login.js
@@ -1,7 +1,20 @@
 const dao = require('../dao/usuarioDAO');
 const argon2 = require('argon2');
 
-module.exports.login = async ({ email, senha }) => {
+module.exports.login = async login => {
+   const { email, senha } = login
+   const senhaCriptografada = await buscarSenha(email)
+   await verificarSeSenhaSaoIguais(senhaCriptografada, senha)
+}
+
+const verificarSeSenhaSaoIguais = async (senhaCriptografada, senha) => {
+   const senhaSaoIguais = await argon2.verify(senhaCriptografada, senha, { hashLength: 10 })
+   if(!senhaSaoIguais){
+      throw { statusCode: 409, msg: 'Email e/ou senha estão incorretos' }
+   }
+}
+
+const buscarSenha = async email => {
    const params = {
       TableName: process.env.nomeTabela,
       ProjectionExpression: "senha",
@@ -11,21 +24,13 @@ module.exports.login = async ({ email, senha }) => {
      }
    };
 
-   let result
-   try {
-      result = await dao.buscarSenha(params)
-   } catch (error) {
-      throw new Error(error)
+   const result = await dao.buscarSenha(params)
+
+   const naoExisteUsuario = result.Count === 0
+   if(naoExisteUsuario){
+      throw { statusCode: 409, msg: 'Email e/ou senha estão incorretos' }
    }
 
    const { senha: senhaCriptografada } = result.Items[0]
-
-   try {
-      if(await argon2.verify(senhaCriptografada, senha, { hashLength: 10 })){
-         return
-      }
-      throw new Error('Dado incorreto')
-   } catch (error) {
-      throw new Error(error)
-   }
+   return senhaCriptografada
 }
