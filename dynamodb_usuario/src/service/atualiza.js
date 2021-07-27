bc508ba456ef4d5ec7209164d4a9919825c2214f
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-07-27 07:18:38 -0300

    atualiza email se outro usuario nao estiver usando

--- dynamodb_usuario/src/service/atualiza.js
+++ dynamodb_usuario/src/service/atualiza.js
@@ -2,14 +2,47 @@ const dao = require('../dao/usuarioDAO');
 const argon2 = require('argon2');
 
 module.exports.atualizar = async usuario => {
-   const { id, nome, email, tipo_de_usuario, endereco } = usuario
-   let { senha } = usuario
+
+   const { senha, email, id } = usuario
+
+   if (await verificarSeEmailExiste(email, id)) {
+      throw new Error('Email ja existe')
+   }
+
+   const senhaCriptografada = await criptografarSenha(senha)
+   usuario.senha = senhaCriptografada
 
    try {
-      senha = await encriptarSenha(senha)
+      return await atualizar(usuario)
    } catch (error) {
       throw new Error(error)
    }
+}
+
+const verificarSeEmailExiste = async (email, id) => {
+   const params = {
+      TableName: process.env.nomeTabela,
+      ProjectionExpression: "email",
+      FilterExpression: "email = :email and id <> :id",
+      ExpressionAttributeValues: {
+         ":email": email,
+         ":id": id,
+      }
+   };
+
+   const result = await dao.buscarPeloEmail(params)
+
+   const emailExiste = result.Count !== 0
+
+   return emailExiste
+}
+
+const criptografarSenha = async senha => {
+   return await argon2.hash(senha, { hashLength: 10 })
+}
+
+const atualizar = async usuario => {
+   const { id, nome, senha, email, tipo_de_usuario, endereco } = usuario
 
    const params = {
       TableName: process.env.nomeTabela,
@@ -17,7 +50,7 @@ module.exports.atualizar = async usuario => {
          'id': id
       },
       UpdateExpression: 'set nome = :nome, email = :email, tipo_de_usuario = :tipo_de_usuario, senha = :senha, endereco.cep = :cep, endereco.estado = :estado, endereco.cidade = :cidade, endereco.bairro = :bairro, endereco.rua = :rua, endereco.numero = :numero, endereco.complemento = :complemento',
-      ExpressionAttributeValues:{
+      ExpressionAttributeValues: {
          ':nome': nome,
          ':email': email,
          ':tipo_de_usuario': tipo_de_usuario,
@@ -30,16 +63,8 @@ module.exports.atualizar = async usuario => {
          ':numero': endereco.numero,
          ':complemento': endereco.complemento,
       },
-      ReturnValues:'ALL_NEW'
+      ReturnValues: 'ALL_NEW'
    };
 
-   try {
-      return await dao.atualizar(params)
-   } catch (error) {
-      throw new Error(error)
-   }
-}
-
-const encriptarSenha = async senha => {
-   return await argon2.hash(senha, { hashLength: 10 })
+   return await dao.atualizar(params)
 }
