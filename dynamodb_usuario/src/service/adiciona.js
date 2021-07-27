82694408bb9c4080b38b7cddca74079f56f0394c
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-07-27 14:38:23 -0300

    usuario: validacao do adiciona

--- dynamodb_usuario/src/service/adiciona.js
+++ dynamodb_usuario/src/service/adiciona.js
@@ -1,9 +1,12 @@
 const dao = require('../dao/usuarioDAO');
 const uuid = require('uuid');
 const argon2 = require('argon2');
+const { schema } = require('../validacao');
 
 module.exports.adicionar = async usuario => {
    let { email, senha } = usuario
+
+   await validar(usuario)
 
    if (await verificarSeEmailExiste(email)) {
       throw { statusCode: 409, msg: "O email ja existe" }
@@ -12,6 +15,22 @@ module.exports.adicionar = async usuario => {
    senha = await criptografarSenha(senha)
 
    await adicionar({ ...usuario, senha })
+}
+
+const validar = usuario => {
+   const result = schema.validate(usuario)
+
+   const naoTemErro = result.error === undefined
+   if (naoTemErro) return
+
+   const erros = result.error.details.map((erro) => {
+      return {
+         campo: erro.context.label,
+         mensagem: erro.message
+      }
+   })
+
+   throw { statusCode: 409, msg: erros }
 }
 
 const criptografarSenha = async senha => {
