1d493d7bd066bfa46d50e4bf863236e58ec4ef59
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-07-29 20:26:22 -0300

    usuario: refatorando deleta

--- dynamodb_usuario/src/service/deleta.js
+++ dynamodb_usuario/src/service/deleta.js
@@ -1,12 +1,5 @@
 const dao = require('../dao/usuarioDAO');
 
 module.exports.deletar = async id => {
-   const params = {
-      TableName: process.env.nomeTabela,
-      Key: {
-         "id": id
-      }
-   };
-
-   await dao.deletar(params)
+   await dao.deletar(id)
 }
