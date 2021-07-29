d244ce40d1231a6fafb051e2b9f696f99196d1dd
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-07-29 20:30:40 -0300

    pedido: refatorando adiciona

--- dynamodb_pedido/src/controller/adiciona.js
+++ dynamodb_pedido/src/controller/adiciona.js
@@ -3,10 +3,7 @@ const service = require('../service/adiciona');
 module.exports.adicionar = async (event) => {
    const { pedido } = JSON.parse(event.body);
 
-   console.log(pedido)
-
    let response = {
-      statusCode: 0,
       headers: {
          'Access-Control-Allow-Origin': '*',
          'Access-Control-Allow-Credentials': true,
@@ -14,18 +11,16 @@ module.exports.adicionar = async (event) => {
    }
 
    try {
-      const url_payment = await service.adicionar(pedido)
+      const urlPayment = await service.adicionar(pedido)
 
       response.statusCode = 200
-      response.body = JSON.stringify({ url_payment: url_payment })
+      response.body = JSON.stringify({ urlPayment })
    } catch (error) {
       console.log(error)
 
       response.statusCode = 500
-      response.body = JSON.stringify({ msg: 'Falha em algo.' })
+      response.body = JSON.stringify({ error: error.msg || 'Falha em algo.' })
    }
-
-   console.log(response)
 
    return response
 }
