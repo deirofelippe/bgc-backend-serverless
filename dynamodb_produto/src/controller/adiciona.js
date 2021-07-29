4f6fc10d539e062a54921a7d7b4edffa19e1daba
felippedesouza <sergiofelippe.deiro@gmail.com>
2021-07-29 20:56:46 -0300

    produto: refatorando adiciona

--- dynamodb_produto/src/controller/adiciona.js
+++ dynamodb_produto/src/controller/adiciona.js
@@ -1,43 +1,24 @@
-const dao = require('../dao/produto_dao');
-const uuid = require('uuid');
-// const formidable = require('formidable');
+const service = require('../service/adiciona');
 
-module.exports.adicionar = async (event, context) => {
+module.exports.adicionar = async event => {
    const produto = JSON.parse(event.body);
-   // const form = new formidable.IncomingForm()
-   // const result = await form.parse(event.body)
-
-   const params = {
-      TableName: process.env.tabelaProduto,
-      Item: {
-         id: uuid.v1(),
-         // id: '01',
-         nome: produto.nome, 
-         descricao: produto.descricao,
-         preco: produto.preco,
-         imagem: '',
-         createdAt: Date.now(),
-      },
-   };
 
    let response = {
-      statusCode: 0,
       headers: {
          'Access-Control-Allow-Origin': '*',
          'Access-Control-Allow-Credentials': true,
-      },
-      body: ''
+      }
    }
 
    try {
-      await dao.adicionar(params)
+      await service.adicionar(produto)
 
       response.statusCode = 201
    } catch (error) {
       console.log(error)
 
-      response.statusCode = 500
-      response.body = JSON.stringify({ msg: 'Falha em algo.' })
+      response.statusCode = error.statusCode || 500
+      response.body = JSON.stringify({ error: error.msg || 'Algo deu errado' })
    }
 
    return response
